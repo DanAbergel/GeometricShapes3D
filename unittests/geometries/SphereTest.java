d23b92f2174422b20a412747fa6c1a5aabe9be1c
Dan Abergel <dabergel@g.jct.ac.il>
2020-04-02 18:19:01 +0300

    dan

---
+++ unittests/geometries/SphereTest.java
@@ -0,0 +1,18 @@
+package geometries;
+
+import org.junit.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import static org.junit.Assert.*;
+
+public class SphereTest {
+
+    @Test
+    public void getNormal() {
+        Sphere sphere = new Sphere(Point3D.ZERO, 5);
+        Vector normal = sphere.getNormal(new Point3D(1, 1, 1));
+        assertEquals("error", normal, new Vector(1/Math.sqrt(3),1/Math.sqrt(3),1/Math.sqrt(3)));
+    }
+}
