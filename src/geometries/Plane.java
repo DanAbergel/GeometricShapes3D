86f53ed78538d365a2822eda2f26d78fd42c564b
Dan Abergel <dabergel@g.jct.ac.il>
2020-04-23 20:10:14 +0300

    fghjkhjk

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -64,15 +64,11 @@ public class Plane implements Geometry {
         } catch (IllegalArgumentException e) {
             return null; // ray starts from point Q - no intersections
         }
-
         double nv = _normal.dotProduct(ray.getVector());
         if (isZero(nv)) // ray is parallel to the plane - no intersections
             return null;
-
         double t = alignZero(_normal.dotProduct(p0Q) / nv);
-
         Point3D newPoint=ray.getTargetPoint(t);
-
         return t <= 0 || newPoint==ray.getPoint() ? null : List.of(newPoint);//if the point of intersection is the same point than the point of ray return null
     }
 }
