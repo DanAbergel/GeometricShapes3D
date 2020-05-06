f8c897e694fcfd969a3564c34db921c86ebc4967
Dan Abergel <dabergel@g.jct.ac.il>
2020-05-06 19:44:56 +0300

    fghjkhjk

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,5 +1,19 @@
 package renderer;
 
+import org.junit.Test;
+
+import java.awt.*;
+
+
 public class ImageWriterTest {
+    String ImageName="Address";
+    int width=1600;
+    int height=1000;
+    int Nx=800;
+    int Ny=500;
+    ImageWriter image=new ImageWriter(ImageName,width,height,Nx,Ny);
+
+    
+
 
 }
