cd9eb50efb760091d6ba594eb66876729ee2d42b
Dan Abergel <dabergel@g.jct.ac.il>
2020-05-06 11:10:16 +0300

    fghjkhjk

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -5,10 +5,12 @@ import primitives.Color;
 public class AmbientLight {
 
     Color _intensity;
-    double k;
 
     public AmbientLight(Color color, double k) {
-        this.color = color;
-        this.k = k;
+        this._intensity =color.scale(k);
+    }
+
+    public Color get_intensity() {
+        return _intensity;
     }
 }
