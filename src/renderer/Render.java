b68078b0fff115c14e35fad085665a824ee7175e
joss lalou <jossleboss26@gmail.com>
2020-05-08 11:39:41 +0300

    joss est la !

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -1,8 +1,29 @@
 package renderer;
 
+import primitives.Color;
+import primitives.Point3D;
 import scene.Scene;
 
 public class Render {
-    Scene scene;
-    ImageWriter image;
+   private Scene scene;
+   private ImageWriter image;
+   public Render(ImageWriter _imageWriter, Scene _scene)
+   {
+        this.image = _imageWriter;
+        this.scene = _scene;
+   }
+    private Color calcColor(Point3D point)
+    {
+        return scene.getAmbientLight().get_intensity();
+    }
+    public void printGrid(int _interval, java.awt.Color _color)
+    {
+        for (int i = 0; i < this.image.getNx(); ++i)
+            for (int j = 0; j < this.image.getNy(); ++j)
+            {
+                if (j % _interval == 0 || i % _interval == 0)
+                    image.writePixel(j, i, _color);
+            }
+    }
+
 }
