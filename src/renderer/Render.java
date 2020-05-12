3cfdb51c61a57eca58c83e614092dada539723b6
joss lalou <jossleboss26@gmail.com>
2020-05-12 18:50:57 +0300

    joss est la !

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -6,7 +6,10 @@ import primitives.Ray;
 import scene.Scene;
 
 import java.util.List;
-
+/**
+ * Dan Abergel and יהושע לאלו
+ this class aims to represent the whole structure
+ */
 public class Render {
    private Scene scene;
    private ImageWriter image;
@@ -15,10 +18,21 @@ public class Render {
         this.image = _imageWriter;
         this.scene = _scene;
    }
+    /**
+     * Calculate the color intensity in a point
+     *
+     * @param point intersection the point for which the color is required
+     * @return the color intensity
+     */
     private Color calcColor(Point3D point)
     {
         return scene.getAmbientLight().get_intensity();
     }
+    /**
+     * Printing the grid with a fixed interval between lines
+     *
+     * @param _interval The interval between the lines.
+     */
     public void printGrid(int _interval, java.awt.Color _color)
     {
         for (int i = 0; i < this.image.getNx(); ++i)
@@ -28,6 +42,13 @@ public class Render {
                     image.writePixel(j, i, _color);
             }
     }
+    /**
+     * Finding the closest point to the P0 of the camera.
+     *
+     * @param intersectionsPoints list of points, the function should find from
+     *                           this list the closet point to P0 of the camera in the scene.
+     * @return the closest point to the camera
+     */
     private Point3D getClosestPoint(List<Point3D> intersectionsPoints){
        double distance=Double.MAX_VALUE;
        Point3D P0=scene.getCamera().getPlace();
@@ -41,8 +62,13 @@ public class Render {
        }
        return minDistancePoint;
     }
+    /**
+     * Filling the buffer according to the geometries that are in the scene.
+     * This function does not creating the picture file, but create the buffer pf pixels
+     */
     public void renderImage()
     {
+        //pixels grid
         for (int i=0;i<image.getNx();i++)
         {
             for (int j=0;j<image.getNy();j++)
